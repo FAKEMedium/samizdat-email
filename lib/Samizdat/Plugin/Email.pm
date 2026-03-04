b515f7a0f0c01bd2d0bc64c9dc39bb5d7e6b0437
Hans Svensson <hans@rymdweb.com>
2026-03-04 01:24:12 +0100

    Swish Commerce fixes Restrict loading of plugins to require configuration.

--- lib/Samizdat/Plugin/Email.pm
+++ lib/Samizdat/Plugin/Email.pm
@@ -5,6 +5,8 @@ use Samizdat::Model::Email;
 use Mojo::Loader qw(data_section);
 
 sub register ($self, $app, $conf) {
+  return if (!(exists($app->config->{manager}->{email})));
+
   my $r = $app->routes;
 
   # Store OpenAPI fragment (parsed centrally in _load_openapi)
